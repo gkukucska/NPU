36377a8394dc61809c71757dc41c76b0964ecdf4
Gergő Kukucska <gkukucska91@gmail.com>
2023-05-31 17:20:42 +0200

    Loadpage visibility modifications

--- Modules/GUI/Pages/LoginPage/LoginPageViewModel.cs
+++ Modules/GUI/Pages/LoginPage/LoginPageViewModel.cs
@@ -5,16 +5,22 @@ using CommunityToolkit.Mvvm.Input;
 namespace NPU.GUI.LoginPage
 {
     // All the code in this file is included in all platforms.
-    public partial class LoginPageViewModel:ObservableObject
+    public partial class LoginPageViewModel : ObservableObject
     {
         private IAuthenticatorClient _aurthenticatorClient;
         private IRegistrationClient _registrationClient;
 
-        public LoginPageViewModel(IAuthenticatorClient authenticatorClient,IRegistrationClient registrationClient)
+        public LoginPageViewModel(IAuthenticatorClient authenticatorClient, IRegistrationClient registrationClient)
         {
             _aurthenticatorClient = authenticatorClient;
             _registrationClient = registrationClient;
         }
+
+        [ObservableProperty]
+        private bool _isLogedIn;
+
+        [ObservableProperty]
+        private string _status;
 
         [ObservableProperty]
         private string _username;
@@ -25,15 +31,48 @@ namespace NPU.GUI.LoginPage
         [RelayCommand]
         private async void Login(object parameters)
         {
-            var data = (List<string>)parameters;
-            await _aurthenticatorClient.OpenSessionAsync(data[0], data[1]);
+            Status=string.Empty;
+            try
+            {
+                var data = (List<string>)parameters;
+                await _aurthenticatorClient.OpenSessionAsync(data[0], data[1]);
+                IsLogedIn = true;
+            }
+            catch (Exception)
+            {
+                Status = "Failed to log in";
+                IsLogedIn=false;
+            }
         }
 
         [RelayCommand]
         private async void Register(object parameters)
         {
-            var data = (List<string>)parameters;
-            await _registrationClient.RegisterAsync(data[0], data[1]);
+            Status = string.Empty;
+            try
+            {
+                var data = (List<string>)parameters;
+                await _registrationClient.RegisterAsync(data[0], data[1]);
+            }
+            catch (Exception e)
+            {
+                Status = "Failed to register";
+            }
+        }
+
+        [RelayCommand]
+        private async void ValidateRegistrationData(object parameters)
+        {
+            Status = string.Empty;
+            try
+            {
+                var data = (List<string>)parameters;
+                await _registrationClient.ValidateRegistrationDataAsync(data[0], data[1]);
+            }
+            catch (Exception e)
+            {
+                Status = "Registration failed, invalid username or password";
+            }
         }
     }
 }
---
+++ Solutions/NPUMobileFrontend/NPUMobileFrontend/Converters/BoolInvertConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NPU.MobileFrontend.Converters
+{
+    internal class BoolInvertConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return !(value as bool?) ?? Binding.DoNothing;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return !(value as bool?) ?? Binding.DoNothing;
+        }
+    }
+}
